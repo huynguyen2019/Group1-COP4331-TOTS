1e8ab0bcb360e39a587345e8517c18291478ede4
Christine Stevens <christinemstevens02@gmail.com>
2022-03-27 20:32:55 -0400

    server

--- server.js
+++ server.js
@@ -25,16 +25,16 @@ mongoose.connect(process.env.MONGO_CONNECTION_STRING, {
 });
 const app = express();
 app.use(bodyParser.json());
-// app.use("/", express.static("frontend/public"));
-app.use(express.static(path.resolve(__dirname, '../client/build')));
+
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.get("/api", (req, res) => {
   res.json({ message: "Hello from server!" });
 });
 
-app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
-});
+// app.get('/', (req, res) => {
+//   res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+// });
 
 app.use(session({
 	secret: "foo",
@@ -303,6 +303,10 @@ app.get("/session", (req, res) =>
 {
 	res.send(req.session);
 });
+
+app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname+'/client/build/index.html'));
+  });
 
 let port = process.env.PORT || 5000; 
 app.listen(port, () =>
